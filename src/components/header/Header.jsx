ffe779e2510e391ff3ada45c6088965bc87d13a1
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-07-24 08:22:06 -0500

    Fix: Added SearchBar component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,124 +1,69 @@
-import React, { useState, useRef, useEffect } from "react";
-// Css header
+import React, { useState } from "react";
+// Style imports
 import "../../components/header/headerStyles.css";
-// Logo riwi
+// Logo imports
 import logo from "../../assets/logo/Logos.webp";
-// Modal
+// Import of section start modal
 import { ModalSesion } from "../modals-sesions/modalLogin.jsx";
-
-
-let SearchEd = "";
+// Import Search bar
+import { SearchBar } from "../search-bar/SearchBar.jsx";
 
 export const Header = () => {
-
-  // Para abrir el modal de user
-  const [isModalLoginOpen, setIsModalLoginOpen] = useState(false);
-
-  // Para que el icono del user cambie de color
-  const [isUserFocused, setIsUserFocused] = useState(false);
-
-  //---------------
-  // Para que el icono del carrito cambie de color
-  const [isCartFocused, setIsCartFocused] = useState(false);
-
-  useEffect(() => {
-    if(isModalLoginOpen){
-      setIsCartFocused(false)
-    }
   
-    if(isCartFocused){
-      setIsModalLoginOpen(false)
-      setIsUserFocused(false)
-
-    }
-
-  }, [isModalLoginOpen,isCartFocused]);
-  //------------------
-
-  // Para abrir el menu hamburguesa
+  // Status to open the user modal
+  const [isModalLoginOpen, setIsModalLoginOpen] = useState(false);
+  // Status to change color of the section start button
+  const [isUserFocused, setIsUserFocused] = useState(false);
+  // Status to change the color of the cart start button
+  const [isCartFocused, setIsCartFocused] = useState(false);
+  // Status to open the hamburger menu
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  // Para que el icono de la barra de busqueda cambie de color
-  const [isSearchFocused, setIsSearchFocused] = useState(false);
 
-  //-------------------------------------
-  //guardar valor de busqueda escrito
-  const [searched, SetSearched] = useState("");
-
-  // Funcion para el icono del login
+  // Function to open the modal and make the button change color
   const handleLoginClick = () => {
     setIsModalLoginOpen(!isModalLoginOpen);
     setIsUserFocused(!isUserFocused);
   };
 
-  //Logica para detectar enter y guardar la b
-  const keyPress = (event) => {
-    if (event.key === "Enter") {
-      SearchEd !== "" ? localStorage.setItem("search" , SearchEd): null;
-      window.location.href = "/search"
-    }
-  };
-  const handleChanged = (event) => {
-    SearchEd = event.target.value;
-    SetSearched(event.target.value);
-  
-    
-  };
-  return ( 
+  return (
+    <> 
+      {/**Logo */}
+      <header className="header relative z-50">
+        <div className="logo">
+          <a href="/">
+            <img src={logo} alt="Kiwi logo" className="logo-img" />
+          </a>
+        </div>
 
-    <header className="header relative z-50">
-      {/*Logo*/}
-      <div className="logo">
-        <a href="/">
-          <img src={logo} alt="Kiwi logo" className="logo-img" />
-        </a>
-      </div>
+        {/**Component Search bar */}
+        <SearchBar isMenuOpen={isMenuOpen} />
 
-      {/*Barra de busqueda*/}
-      <div className={`search-bar ${isMenuOpen ? "open" : ""}`}>
-        <input
-          type="text"
-          placeholder="Search"
-          className={isSearchFocused ? "focused" : ""}
-          onFocus={() => setIsSearchFocused(true)}
-          onBlur={() => setIsSearchFocused(false)}
+        {/**Shopping cart button */}
+        <div className={`icons ${isMenuOpen ? "open" : ""}`}>
+          <button
+            onClick={() => setIsCartFocused(!isCartFocused)}
+            className={`icon-btn cart-btn ${isCartFocused ? "focused" : ""}`}
+          >
+            <i className="fas fa-shopping-cart icon cart"></i>
+          </button>
 
-          //param of keypress
-              onChange={handleChanged}
-              onKeyDown={keyPress}
-        />
-        <i className={`fas fa-search search-icon ${isSearchFocused ? "focused-icon" : ""}`}></i>
-      </div>
+          {/**Section start button */}
+          <button
+            onClick={handleLoginClick}
+            className={`icon-btn user-btn ${isUserFocused ? "focused" : ""}`}
+          >
+            <i className="fas fa-user icon user"></i>
+          </button>
+        </div>
 
-      {/*Iconos del carrito y user*/}
-      <div className={`icons ${isMenuOpen ? "open" : ""}`}>
-
-        <button
-          onClick={() => setIsCartFocused(!isCartFocused)}
-          className={`icon-btn cart-btn ${isCartFocused ? "focused" : ""}`}
-        >
-          <i className="fas fa-shopping-cart icon cart"></i>
+        {/**Button to open hamburger menu */}
+        <button className="menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <i className="fas fa-bars icon"></i>
         </button>
 
-        <button
-          onClick={handleLoginClick}
-          className={`icon-btn user-btn ${isUserFocused ? "focused" : ""}`}
-
-        >
-          <i className="fas fa-user icon user"></i>
-        </button>
-      </div>
-
-      {/*Boton para abrir el menu hamburguesa*/}
-      <button className="menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        <i className="fas fa-bars icon"></i>
-      </button>
-
-      {/*Condicional para mostrar el modal*/}      
-      {isModalLoginOpen && (
-        <ModalSesion
-          active={isModalLoginOpen}
-        />
-      )}
-    </header>
+        {/*Conditional to show modal*/}   
+        {isModalLoginOpen && <ModalSesion active={isModalLoginOpen} />}
+      </header>
+    </>
   );
 };
---
+++ src/components/search-bar/SearchBar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+// Style imports
+import "../../components/header/headerStyles.css";
+
+export const SearchBar = ({ isMenuOpen }) => {
+
+  // Status to change the color of the search bar
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  // Status to save the search value
+  const [searched, setSearched] = useState("");
+
+  // Function to manage changes in input
+  const handleChanged = (event) => {
+    setSearched(event.target.value);
+  };
+
+  // Logic for the bar to search after hitting enter 
+  const keyPress = (event) => {
+    if (event.key === "Enter" && searched !== "") {
+      localStorage.setItem("search", searched);
+      window.location.href = "/search";
+    }
+  };
+
+  return (
+    <div className={`search-bar ${isMenuOpen ? "open" : ""}`}>
+      <input
+        type="text"
+        placeholder="Search"
+        className={isSearchFocused ? "focused" : ""}
+        onFocus={() => setIsSearchFocused(true)}
+        onBlur={() => setIsSearchFocused(false)}
+        //Params of keypress 
+        onChange={handleChanged}
+        onKeyDown={keyPress}
+      />
+      <i className={`fas fa-search search-icon ${isSearchFocused ? "focused-icon" : ""}`}></i>
+    </div>
+  );
+};
