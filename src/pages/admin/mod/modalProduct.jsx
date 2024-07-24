4650d719d846d15bff6fc5de70b1a434a0e91485
SantiPelaez07 <santipelaez0@gmail.com>
2024-07-24 08:36:09 -0500

    Corrections in modals admin

---
+++ src/pages/admin/mod/modalProduct.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useRef, useEffect } from "react";
+
+
+export const ProductModalParam = ({ active }) => {
+
+    const [activeModal, SetactiveModal] = useState(false);
+
+    useEffect(() => {
+        SetactiveModal(active == true)
+    }, [activeModal]);
+
+    if (activeModal) {
+        return (
+            <div></div>
+        )
+    } else {
+        return (
+
+            <div className="modal fade" id="userModal">
+                <h1>Modal activo</h1>
+            </div>
+        )
+    }
+
+}
